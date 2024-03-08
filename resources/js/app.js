6837d0edc141d80323cf3f1d2a2c54103ef8df28
audiverth77 <audiverth77@gmail.com>
2024-03-07 22:51:31 -0500

    solucion para el comando npm run build

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,6 @@
 import { createApp } from 'vue';
-import App from './components/App.vue';
-import router from './router';
 import './bootstrap';
 import '../css/app.css';
 
 createApp(App)
-  .use(router)
   .mount('#app');
